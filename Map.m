7699a1e2c88200ae9173d8cd24f039048b58c670
Gaben11 <juvefanmaster@gmail.com>
2014-12-31 13:53:26 +0100

    Map class definition
    Created the map class skeleton.

---
+++ Map.m
@@ -0,0 +1,16 @@
+classdef Map
+    
+    properties
+        bugs = {}; % bugs on the map
+        foodSupply = {}; % food on the map
+        obstacles = {}; % obstacles on the map
+        size = [100 100]; % map size is 100*100 
+    end
+    
+    methods
+        function obj = Map
+        
+        end
+    end
+    
+end
