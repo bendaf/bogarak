5f4021a08703017b7f359abae54e796e2c249344
Gaben11 <juvefanmaster@gmail.com>
2014-12-31 13:23:39 +0100

    Obstacle class
    Created obstacle class with a constructor. Other methods are not
    necessary.

---
+++ Obstacle.m
@@ -0,0 +1,23 @@
+classdef Obstacle
+   
+    properties
+        pos = [0 0]; % this position is the upper left corner position
+        size = 1; % unit size obstacles
+    end
+    
+    methods
+        function obj = Obstacle(pos, size) % obstacle contructor method
+            switch nargin
+                case 2
+                    obj.pos = pos;
+                    obj.size = size;
+                case 1
+                    obj.pos = pos;
+                    obj.size = round(rand*10);
+                otherwise
+                    obj.pos = round(rand*100);
+                    obj.size = round(rand*10);
+            end
+        end
+    end 
+end
