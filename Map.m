9899bf3c986cd7883a72b1b6be3fc5e4b6e4b4d9
Gaben11 <juvefanmaster@gmail.com>
2015-01-20 16:16:47 +0100

    Modified Map class
    Skeleton of the map class has been updated. Cell arrays are replaced
    with regular vectors.

--- Map.m
+++ Map.m
@@ -3,47 +3,47 @@ classdef Map
     properties
         size = [100 100];   % map size is 100*100 
         
-        bugs = {};          % bugs on the map
-        foodSupply = {};    % food on the map
-        obstacles = {};     % obstacles on the map
+        bugs = [];          % bugs on the map
+        foodSupply = [];    % food on the map
+        obstacles = [];     % obstacles on the map
     end
     
     methods
         function obj = Map
-            % initialize other properties of the map
-            % creates a 100 by 100 cell array with no contents
-            obj.bugs = cell(obj.size(1),obj.size(2));
-            obj.foodSupply = cell(obj.size(1),obj.size(2));
-            obj.obstacles = cell(obj.size(1),obj.size(2));
-            
-            % number of obstacles
+            % n number of obstacles will be placed on the map
             n = round(rand*4)+1;
             % this for loop adds random positioned obstacles to the map
             for i = 1:n
                 x = round(rand*obj.size(1))+1;
                 y = round(rand*obj.size(2))+1;
-                while obj.obstacles{x,y} ~= 0
-                    x = round(rand*obj.size(1))+1;
-                    y = round(rand*obj.size(2))+1;
-                end
-                obj.obstacles{x,y} = Obstacle;
+                obj.obstacles = [obj.obstacles Obstacle([x y],1)];
             end
         end
         
         % plot the entire map
-%         function plotMap()
-% 
-%         end
+        function plotMap(self)
+            
+        end
         
         % adds an insect to swarm
-%         function addInsect()
-%             
-%         end
+        function addInsect(self)
+            
+        end
           
         % adds food at random position on the map
-%         function addFood
-%         
-%         end
+        function addFood(index)
+        
+        end
+        
+        % removes an insect from a certain position (index)
+        function removeInsect(index)
+            % 
+        end
+        
+        % removes a food object which has been eaten by an insect
+        function removeFood(index)
+        
+        end
     end
     
 end
